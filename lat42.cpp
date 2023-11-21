51ea54ae4a0f4a51082798b60112dbd604f2a9ca
NyMason <151601124+NyMason@users.noreply.github.com>
2023-11-20 23:39:23 -0800

    Update lat42.cpp

--- lat42.cpp
+++ lat42.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-int permutasi (int x, int y) {
+int permutasi(int x, int y) {
     int nilai_permut, selisih;
     int faktorial = 1;
     selisih = x - y;
@@ -15,7 +15,7 @@ int permutasi (int x, int y) {
         return(nilai_permut);
     }
     
-    int main () {
+int main () {
         int n, r;
         cout << "masukkan nilai n: ";
         cin >> n;
@@ -25,13 +25,13 @@ int permutasi (int x, int y) {
         }
         cout << "masukkan nilai r: ";
         cin >> r;
-        while (r < 0);
+        while (r < 0){
         cout << "masukkan nilai r kembali: ";
         cin >> r;
         cin >> r;
     }
     
-    cout << "nilai permutasi " << r << " dari " << n << permutasi(n, r) << endl;
+    cout << "nilai permutasi " << r << " dari " << n << " adalah " << permutasi(n, r) << endl;
     
     system("PAUSE");
     return(0);
