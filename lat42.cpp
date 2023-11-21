3032bb758e26517a98f45a03a9dcffa026b10332
NyMason <151601124+NyMason@users.noreply.github.com>
2023-11-20 23:29:51 -0800

    upload file

---
+++ lat42.cpp
@@ -0,0 +1,40 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int permutasi (int x, int y) {
+    int nilai_permut, selisih;
+    int faktorial = 1;
+    selisih = x - y;
+    
+    while (x > 0){
+        faktorial = faktorial * x;
+         x = x-1;
+        }
+        nilai_permut = faktorial/selisih;
+        return(nilai_permut);
+    }
+    
+    int main () {
+        int n, r;
+        cout << "masukkan nilai n: ";
+        cin >> n;
+        while (n < 0){
+         cout << "masukkan nilai N kembali: ";
+         cin >> n;
+        }
+        cout << "masukkan nilai r: ";
+        cin >> r;
+        while (r < 0);
+        cout << "masukkan nilai r kembali: ";
+        cin >> r;
+        cin >> r;
+    }
+    
+    cout << "nilai permutasi " << r << " dari " << n << permutasi(n, r) << endl;
+    
+    system("PAUSE");
+    return(0);
+    
+}
+    
